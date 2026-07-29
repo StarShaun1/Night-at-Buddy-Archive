d153a42482fbcc25f2fd1667850dada7efa15a61
star_shaun <69088477+StarShaun1@users.noreply.github.com>
2026-07-29 01:06:57 -0300

    Update shaunscript.js

--- other/shaunscript.js
+++ other/shaunscript.js
@@ -1,1 +1,5 @@
+//please don't kill me
+
 var orange = Math.floor(Math.random() * (10 - 1 + 1)) + 1; if (orange === 1) { alert("Give orange"); } else { console.log("Awww, no orange message " + orange); } playBeeGees(); function playBeeGees() { var beegees = new Audio("https://starshaun1.github.io/Night-at-Buddy-Archive/downloads/beegees.mp3"); beegees.play(); } 
+
+document.write("aah");
