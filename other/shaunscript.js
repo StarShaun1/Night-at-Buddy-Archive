440a29e9f8aad512da6348f6624f322a95eab88a
star_shaun <69088477+StarShaun1@users.noreply.github.com>
2026-07-29 01:10:03 -0300

    Update shaunscript.js

--- other/shaunscript.js
+++ other/shaunscript.js
@@ -1,5 +1,20 @@
-//please don't kill me
 
-var orange = Math.floor(Math.random() * (10 - 1 + 1)) + 1; if (orange === 1) { alert("Give orange"); } else { console.log("Awww, no orange message " + orange); } playBeeGees(); function playBeeGees() { var beegees = new Audio("https://starshaun1.github.io/Night-at-Buddy-Archive/downloads/beegees.mp3"); beegees.play(); } 
+// please don't kill me
+window.open('https://cdn.discordapp.com/attachments/1502350960280997910/1529599718454202408/IMG_8194.png?ex=6a6a6f3f&is=6a691dbf&hm=797754a0f43644cf84f5f8e87caf8e07ab84d6fb609968ee0ca522fc9e0275f9&', '_blank');
 
-document.write("aah");
+var orange = Math.floor(Math.random() * (10 - 1 + 1)) + 1; 
+
+if (orange === 1) { 
+  alert("Give orange"); 
+} else { 
+  console.log("Awww, no orange message " + orange); 
+} 
+
+playBeeGees(); 
+
+function playBeeGees() { 
+  var beegees = new Audio("https://starshaun1.github.io/Night-at-Buddy-Archive/downloads/beegees.mp3"); 
+  beegees.play(); 
+} 
+
+// ahh
