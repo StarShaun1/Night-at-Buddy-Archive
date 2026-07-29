ca79501abf48a3ca6ca796e6821104d093d364c1
star_shaun <69088477+StarShaun1@users.noreply.github.com>
2026-07-29 11:14:58 -0300

    Update shaunscript.js

--- other/shaunscript.js
+++ other/shaunscript.js
@@ -1,6 +1,7 @@
 
 // please don't kill me
-window.open('https://cdn.discordapp.com/attachments/1502350960280997910/1529599718454202408/IMG_8194.png?ex=6a6a6f3f&is=6a691dbf&hm=797754a0f43644cf84f5f8e87caf8e07ab84d6fb609968ee0ca522fc9e0275f9&', '_blank');
+
+alert("Welcome to my profile");
 
 var orange = Math.floor(Math.random() * (10 - 1 + 1)) + 1; 
 
